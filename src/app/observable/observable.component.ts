78efbf7d57114b69a8f2876af8f0d0db3a6476df
Souvik Basu <souvikbasu@gmail.com>
2020-09-06 01:39:21 -0700

    feat(observable): show an item moving through the observable every second

--- src/app/observable/observable.component.ts
+++ src/app/observable/observable.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'rk-observable',
   templateUrl: './observable.component.html',
   styleUrls: ['./observable.component.css']
 })
-export class ObservableComponent implements OnInit {
-
-  constructor() { }
+export class ObservableComponent implements OnInit, OnDestroy {
+  timer: number;
+  dataIndex = 0;
 
   ngOnInit(): void {
+    this.timer = setInterval(() => {
+      this.dataIndex++;
+    }, 1000);
   }
 
+  ngOnDestroy(): void {
+    clearInterval(this.timer);
+  }
 }
