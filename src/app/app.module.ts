a99079c13915925303d8155d570ba0ab1ac2905d
Souvik Basu <souvikbasu@gmail.com>
2020-09-06 01:21:56 -0700

    feat(observable): show static observable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ObservableComponent } from './observable/observable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ObservableComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/observable/observable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rk-observable',
+  templateUrl: './observable.component.html',
+  styleUrls: ['./observable.component.css']
+})
+export class ObservableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
