2becc5f1ed4e0a488f93030c30c933e00f9c1d1c
Souvik Basu <souvikbasu@gmail.com>
2020-10-18 04:29:19 -0700

    feat(streams): add a graphical stream to show colors based on whether the stream is live or not

---
+++ src/app/streams/graphical-stream/graphical-stream.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'rk-graphical-stream',
+  templateUrl: './graphical-stream.component.html',
+  styleUrls: ['./graphical-stream.component.css']
+})
+export class GraphicalStreamComponent implements OnChanges {
+  @Input() public marble: string;
+  @Input() public length: number;
+  @Output() public added =  new EventEmitter();
+  public isLive = false;
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes.marble && changes.marble.currentValue) {
+        const currentMarble = changes.marble.currentValue;
+        this.isLive = true;
+        setTimeout(() => {
+          this.added.emit(new CustomEvent('added', { detail: currentMarble}));
+          this.isLive = false;
+        }, 1000);
+    }
+  }
+}
