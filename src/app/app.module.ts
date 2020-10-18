ee9d84eed618ff821a791e10285ea8743e62f9c4
Souvik Basu <souvikbasu@gmail.com>
2020-10-18 02:55:31 -0700

    fix(app): create triggers and outputs folders to arrange components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ConsoleOutputComponent } from './console-output/console-output.component';
-import { ManualTriggerComponent } from './manual-trigger/manual-trigger.component';
+import { ConsoleOutputComponent } from './outputs/console-output/console-output.component';
+import { ManualTriggerComponent } from './triggers/manual-trigger/manual-trigger.component';
 import { ObservableComponent } from './observable/observable.component';
 import { StreamComponent } from './stream/stream.component';
-import { TimerTriggerComponent } from './timer-trigger/timer-trigger.component';
+import { TimerTriggerComponent } from './triggers/timer-trigger/timer-trigger.component';
 
 @NgModule({
   bootstrap: [AppComponent],
