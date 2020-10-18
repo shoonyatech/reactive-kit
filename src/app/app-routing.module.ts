a4b276f999064f26225a7da878b860b712859a80
Souvik Basu <souvikbasu@gmail.com>
2020-10-18 01:27:57 -0700

    feat(app): add support for routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes: Routes = [];
+
+@NgModule({
+  exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)],
+})
+export class AppRoutingModule { }
