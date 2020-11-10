3584eab633114ecc1ff0ed960d757cfc5021d184
Souvik Basu <souvikbasu@gmail.com>
2020-11-10 13:38:07 -0800

    feat(windows): add empty code window and show in the app layout

---
+++ src/app/windows/code-window/code-window.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rk-code-window',
+  templateUrl: './code-window.component.html',
+  styleUrls: ['./code-window.component.css']
+})
+export class CodeWindowComponent implements OnInit {
+  @Input() code: string;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
