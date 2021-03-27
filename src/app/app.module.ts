fb44f220ff5c1e079059a47c86579f00742b0ba8
Souvik Basu <souvikbasu@gmail.com>
2021-03-27 16:11:46 -0700

    fix(angular): add scss support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConsoleOutputComponent } from './outputs/console-output/console-output.component';
 import { ManualTriggerComponent } from './triggers/manual-trigger/manual-trigger.component';
@@ -11,7 +12,6 @@ import { ListOutputComponent } from './outputs/list-output/list-output.component
 import { GraphicalStreamComponent } from './streams/graphical-stream/graphical-stream.component';
 
 @NgModule({
-  bootstrap: [AppComponent],
   declarations: [
     AppComponent,
     ObservableComponent,
@@ -22,7 +22,8 @@ import { GraphicalStreamComponent } from './streams/graphical-stream/graphical-s
     ListOutputComponent,
     GraphicalStreamComponent,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
